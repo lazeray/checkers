dff2636b316fa44a0e8e552d1a224970e56ce37a
Aiden <aidenye@berkeley.edu>
2024-08-15 15:13:49 -0700

    cleaned up bad checkers ai structure

--- src/Checkers_AI.py
+++ src/Checkers_AI.py
@@ -2,7 +2,8 @@ import Board
 import random
 
 def get_move(board, player):
-    return random_generator(board.get_all_moves(player)) # swap this line out for future algorithms
+    return random_generator(board, player) # swap this line out for future algorithms
 
-def random_generator(avaliable_moves):
+def random_generator(board, player):
+    avaliable_moves = board.get_all_moves(player)
     return random.choice(avaliable_moves)
